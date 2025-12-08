af74de32d96d71b8f87edbe22a2cab781dcfcbc5
Anna Iosif <anna@ushahidi.com>
2025-12-08 21:06:47 +0100

    Cleaning up code, improving userflow

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 root = tk.Tk()
 root.title("A festive Memory Game")
@@ -11,33 +12,49 @@ instructions = tk.Label(root, text="Memorize the order of the numbers before the
 
 
 def check_sequence():
-    global number_of_tries, userInput, sequence, result_label
+    global number_of_tries, userInput, sequence, submit_button, result_label
     result_label.pack_forget()
     number_of_tries += 1
     user_input = userInput.get()
     user_sequence = list(map(int, user_input.split()))
-    if(user_sequence == sequence):
-        result_label.config(text=f"Correct! You recalled the sequence in {number_of_tries} tries.", font=("Helvetica", 18))
+
+    if user_sequence == sequence:
+        result_label.config(
+            text=f"Correct! You recalled the sequence in {number_of_tries} tries.", 
+            font=("Helvetica", 18)
+        )
+        userInput.pack_forget()
+        submit_button.pack_forget()
+        start_game.config(text="Play Again")
+        start_game.pack(pady=30)
     else:
         userInput.delete(0, tk.END)
-        result_label.config(text=f"Incorrect sequence. You entered {user_input}. You have now guessed {number_of_tries} times. Try again!", font=("Helvetica", 18))
+        result_label.config(
+            text=f"Incorrect sequence. You entered {user_input}. You have now guessed {number_of_tries} times. Try again!", 
+            font=("Helvetica", 18)
+        )
     
     result_label.pack(pady=20)
     
 def start_button():
-    global game_started, userInput
-    game_started = True
+    global sequence, userInput, submit_button, result_label, number_of_tries
+    # cleaning up UI if user is playing again
     start_game.pack_forget()
-    print("Game Started!")
-    global sequence
-    sequence = [1, 2, 3, 4, 5]
+    result_label.pack_forget()
+    number_of_tries = 0
+    
+    # generate random sequence
+    sequence = random.sample(range(1, 11), 5)
     sequence_label = tk.Label(root, text=" ".join(map(str, sequence)), font=("Helvetica", 20))
     sequence_label.pack(pady=20)
-    root.after(3000, sequence_label.destroy)
+    
     userInput = tk.Entry(root, font=("Helvetica", 18))
-    userInput.pack(pady=20)
     submit_button = tk.Button(root, text="Submit", font=("Helvetica", 18), command=check_sequence)
-    submit_button.pack(pady=10)
+    
+    # showing sequence for 3 seconds, then hiding it and showing input field
+    root.after(3000, sequence_label.destroy)
+    root.after(3000, lambda: userInput.pack(pady=20))
+    root.after(3000, lambda: submit_button.pack(pady=20))
 
 start_game = tk.Button(root, text="Start Game", font=("Helvetica", 18), command=start_button)
 start_game.pack(pady=30)
